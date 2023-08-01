7a69c5c0da7beefacc7dbefaf9b85dbbd8bce751
jiwonson1 <jiwonson.dev@gmail.com>
2023-08-01 18:11:48 +0900

    add Aug_01

--- Algorithm/jiwon/July_30_02.py
+++ Algorithm/jiwon/July_30_02.py
@@ -5,32 +5,25 @@
 import heapq
 
 def solution(operations):
-    #최소힙
     answer = []
-    #최대힙
     answer2 = []
     heapq.heapify(answer)
     heapq.heapify(answer2)
     
-    while len(operations)>0:
-        a = operations.pop(0)
-        num = 0
-        if 'I' in a:
-            num = int(a.replace('I ', ''))
-            heapq.heappush(answer, num)
-            heapq.heappush(heap, -value)
-        elif a == "D -1":
-            try: 
-                heapq.heappop(answer)
-            except:
-                pass
-        elif a == "D 1":
-            try:
-                -heapq.heappop(answer2)
-                
-            except:
-                pass
-        if len(answer) == 0 and len(operations) == 0:
-            return [0,0]
-    
-    return [-heapq.heappop(answer), heapq.heappop(answer)]
+    while len(operations) > 0:
+        op = operations.pop(0)
+        if op[0] == "I":
+            heapq.heappush(answer, int(op[1:]))
+            heapq.heappush(answer2, -int(op[1:]))
+        elif len(answer) == 0 and len(answer2) == 0:
+            continue
+        elif op == "D 1":
+            temp = heapq.heappop(answer2)
+            answer.remove(-temp)
+        else:
+            temp = heapq.heappop(answer)
+            answer2.remove(-temp)
+    if len(answer) == 0 and len(answer2) == 0:
+        return [0, 0]
+    answer = [-heapq.heappop(answer2), heapq.heappop(answer)]
+    return answer
